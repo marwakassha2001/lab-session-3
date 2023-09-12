3baea1e8d8ecb3673e6fac3dfe6da54563183332
whathow <marwa@gmail.com>
2023-09-12 16:33:30 +0300

    finish ex1

---
+++ marwa_anwar_1.js
@@ -0,0 +1,13 @@
+var numberOfKeys = parseInt(prompt("enter the number of keys for to4"));
+switch (numberOfKeys){
+    case 0:
+        alert("we sank together");
+        break;
+    case 30000:
+        alert("we made it");
+        break;
+    default:
+        alert("we need to swim alittle bit more");
+        break;
+        
+}
